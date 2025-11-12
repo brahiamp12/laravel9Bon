26829adbc5dc23d5b5a58c94549a43b1f26b49a0
proyectoInventario <brahiampatino2212@gmail.com>
2025-11-12 09:17:20 -0500

    docs: :sparkles: Incluir componente de conteo

---
+++ resources/views/livewire/counter-component.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <p> Hola </p>
+    <p> Contador: {{ $count }} </p>
+    <button wire:click="increment"> Incrementar </button>
+    <button wire:click="decrement"> Decrementar </button>
+
+</div>
